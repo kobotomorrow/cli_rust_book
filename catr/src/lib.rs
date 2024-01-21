d4b0504ff651f1c335eccd4efe3216b03a5d260d
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-01-21 18:51:29 +0900

    catr: Process args

---
+++ catr/src/lib.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+
+use clap::Parser;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "Rust cat")]
+pub struct  Config {
+    /// input file
+    file: Vec<String>,
+
+    /// Number lines
+    #[arg(short='n', long="number")]
+    number_lines: bool,
+
+    /// Number non-blank lines
+    #[arg(short='b', long="number-nonblank")]
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    Ok(Config::parse())
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
