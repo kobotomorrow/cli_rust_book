995e4259a582083867dfc6c26dccbe534dbd624f
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-01-21 11:52:59 +0900

    echor: Inplement

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "Rust echo")]
+struct Args {
+    /// Input text
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short = 'n')]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    print!("{}{}", args.text.join(" "), if args.omit_newline { "" } else { "\n" });
 }
