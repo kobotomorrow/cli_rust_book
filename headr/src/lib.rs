d87052b2e16f9bf5fa8e3eb4f01d1df110684492
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-01-31 09:55:19 +0900

    headr: complete

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,4 +1,8 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Read},
+};
 
 use clap::Parser;
 
@@ -11,12 +15,12 @@ pub struct Config {
     files: Vec<String>,
 
     /// Number of lines
-    #[arg(short='n', long="lines" , default_value_t = 10, value_parser = clap::value_parser!(u32).range(1..), group = "input")]
-    lines: u32,
+    #[arg(short='n', long="lines" , default_value_t = 10, value_parser = clap::value_parser!(u64).range(1..), group = "input")]
+    lines: u64,
 
     /// Number of bytes
-    #[arg(short='c', long="bytes", value_parser = clap::value_parser!(u32).range(1..), group = "input")]
-    bytes: Option<u32>,
+    #[arg(short='c', long="bytes", value_parser = clap::value_parser!(u64).range(1..), group = "input")]
+    bytes: Option<u64>,
 }
 
 // fn parse_positive_int(val: usize) -> MyResult<usize> {
@@ -46,6 +50,41 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let filenames = config.files;
+    let num_files = filenames.len();
+
+    for (i, filename) in filenames.iter().enumerate() {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!("{}==> {} <==", if i > 0 { "\n" } else { "" }, filename);
+                }
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes);
+                    let mut buffer = vec![0; num_bytes as usize];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
