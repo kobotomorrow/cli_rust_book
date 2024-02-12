7b633b93f460ee2ad2daf800374d0495780e6de1
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-02-12 10:10:50 +0900

    uniqr: Executable without outfile

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -29,14 +29,38 @@ pub fn run(config: Config) -> MyResult<()> {
     let mut file = open(&input_filename).map_err(|e|
         format!("{}: {}", input_filename, e)
     )?;
-    let mut line = String::new();
+    let mut cur_line = String::new();
+    let mut next_line = String::new();
+    let mut count = 1;
+
+    let bytes = file.read_line(&mut cur_line)?;
+    if bytes == 0 {
+        return Ok(())
+    }
     loop {
-        let bytes = file.read_line(&mut line)?;
+        let bytes = file.read_line(&mut next_line)?;
         if bytes == 0 {
+            if config.count {
+                print!("{:>4} {}", count, cur_line);
+            } else {
+                print!("{}", cur_line);
+            }
             break;
         }
-        print!("{}", line);
-        line.clear();
+        let cur = cur_line.lines().collect::<String>();
+        let next = next_line.lines().collect::<String>();
+        if cur != next {
+            if config.count {
+                print!("{:>4} {}", count, cur_line);
+            } else {
+                print!("{}", cur_line);
+            }
+            cur_line = next_line.clone();
+            count = 1;
+        } else {
+            count += 1;
+        }
+        next_line.clear();
     }
     Ok(())
 }
